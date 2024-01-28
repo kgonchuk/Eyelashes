76c830c3896390c8a6a0c15c9efa320eca66b5d3
kgonchuk <123867028+kgonchuk@users.noreply.github.com>
2024-01-28 16:03:03 +0200

    record adapted

--- js/slick.js
+++ js/slick.js
@@ -4,32 +4,32 @@ $(".multiple-items").slick({
   infinite: false,
   speed: 300,
   slidesToShow: 3,
-  slidesToScroll: 2,
+  slidesToScroll: 1,
   dotsClass: "dots-style",
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 1200,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 2,
+        slidesToScroll: 1,
         infinite: true,
         dots: true,
       },
     },
     {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-    {
-      breakpoint: 480,
+      breakpoint: 768,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
       },
     },
+    // {
+    //   breakpoint: 480,
+    //   settings: {
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //   },
+    // },
     // You can unslick at a given breakpoint now by adding:
     // settings: "unslick"
     // instead of a settings object
